c7fee3aed78213e5abc9c4420737c78d74bf69e9
Victor Muresan <muresan.victor.27@gmail.com>
2025-10-28 18:04:59 +0200

    commit for ex1

---
+++ ex01/ex01.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+from typing import List
+
+def group_anagrams(strs: List[str]) -> List[List[str]]:
+    groups = defaultdict(list)
+    for s in strs:
+        count = [0] * 26
+        for ch in s:
+            count[ord(ch) - ord('a')] += 1
+        groups[tuple(count)].append(s)
+    return list(groups.values())
+
+print(group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+print(group_anagrams([""]))
+print(group_anagrams(["a"]))
